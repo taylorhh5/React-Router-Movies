6ff2efb964fe4b0d94dc66e9acf7ecc7ca140701
MWeberLambdaweb19 <mack.webb37@gmail.com>
2019-08-21 13:21:32 -0700

    Last commit is Mack's refactoring

--- client/src/Movies/Movie.js
+++ client/src/Movies/Movie.js
@@ -1,5 +1,6 @@
 import React, { useState, useEffect } from 'react';
 import axios from 'axios';
+import MovieCard from './MovieCard.js';
 
 const Movie = (props) => {
   const [movie, setMovie] = useState(null);
@@ -35,25 +36,10 @@ const Movie = (props) => {
     return <div>Loading movie information...</div>;
   }
 
-  const { title, director, metascore, stars } = movie;
+  
   return (
     <div className="save-wrapper">
-      <div className="movie-card">
-        <h2>{title}</h2>
-        <div className="movie-director">
-          Director: <em>{director}</em>
-        </div>
-        <div className="movie-metascore">
-          Metascore: <strong>{metascore}</strong>
-        </div>
-        <h3>Actors</h3>
-
-        {stars.map(star => (
-          <div key={star} className="movie-star">
-            {star}
-          </div>
-        ))}
-      </div>
+      <MovieCard movie={movie}/>
       <div className="save-button">Save</div>
     </div>
   );
